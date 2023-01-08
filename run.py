b2bd75fb865343afcb81a371b29d88aa6a67adb4
Sam Parker <s4m.prkr@gmail.com>
2023-01-08 21:39:53 +0000

    Updated

--- run.py
+++ run.py
@@ -2,10 +2,7 @@
 """ Importing """
 
 from snake import *
-from time  import sleep
 
 if __name__ == '__main__':
-    game    = Game(game_parameters='parameters/game.yaml', controller=None, display=True, display_parameters='parameters/graphics.yaml')
-    display = game.getDisplay()
-
+    game = Game(game_parameters='parameters/game.yaml', controller=Controller, display=True, display_parameters='parameters/display.yaml')
     game.begin()
