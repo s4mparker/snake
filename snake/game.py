0e70fe41e551a26348d99040a39df3096ef90450
Sam Parker <s4m.prkr@gmail.com>
2023-01-08 21:39:22 +0000

    Tidied up code

--- snake/game.py
+++ snake/game.py
@@ -1,9 +1,9 @@
 
 """ Importing """
 
-from . import Parameters, Messages, Map, Cell, Display, Snake, Direction
+from .      import Parameters, Messages, Map, Cell, Display, Snake, Direction
 from random import choice
-from time import sleep
+from time   import sleep
 
 """ Packaging """
 
@@ -11,47 +11,51 @@ __all__ = ['Game']
 
 class Game:
 
+    """ TODO
+    
+    
+    """
+
     def __init__(self, game_parameters, controller, display=False, display_parameters=None):
         """ Create a new game 
         
         Parameters: TBC
         """
 
-        params          = Parameters(filename=game_parameters)
+        params          = Parameters.import_file(filename=game_parameters)
 
         # Game components
         self.messages   = Messages('game', 'snake', 'apple')
-        self.map        = Map.generate(params.width, params.height)
-        self.snake      = Snake(self.messages, Direction.LEFT, self.map[20][20])
+        self.map        = Map(params.get('WIDTH'), params.get('HEIGHT'))
+        self.snake      = Snake(self.messages, self.map.at(x=params.get('STARTX'), y=params.get('STARTY')))
 
         # Game state
         self.score      = 0
         self.gameover   = False
         self.lengthen   = False
 
-        # IO components
-        self.controller = controller
-        if display and not display_parameters : raise ValueError(f'no display parameters provided')
-        self.display    = Display(display_parameters) if display else None
-        if self.display : self.display.initialize(self, params.width, params.height)
+        # Game controller
+        move = Direction(params.get('INITIAL'))
+        self.controller = controller(period=params.get('PERIOD'), move=move)
 
-    def __iter__(self):
-        """ Return an iterator over the game's map """
-
-        return iter([cell for row in self.map for cell in row])
-
-    def getDisplay(self):
-        """ Return a display for the game """
-
-        return self.display
+        # Game display (if required)
+        if display and not display_parameters:
+            raise ValueError(f'no display parameters provided')
+        elif not display and display_parameters:
+            raise ValueError(f'display parameters provided but no display enabled')
+        
+        self.display = Display(display_parameters=display_parameters) if display else None
+        if self.display : self.display.initialize(game=self)
 
     def begin(self):
         """ Begin execution of the game """
 
-        for i in range(30):
-            move = choice([d for d in Direction])
-            if i % 2 == 0:
-                self.messages.put('test', 'snake')
+        i = 30
+
+        while not self.gameover and i > 0:
+            
+
+            move = self.controller.poll()
             self.snake.update(move)
-            if self.display: self.display.update()
-            sleep(0.2)
+            if self.display : self.display.update()
+            i -= 1
