6053ffbf67795095d050217927280cac29158a70
Sam Parker <s4m.prkr@gmail.com>
2023-01-08 21:38:50 +0000

    Simplified Snake classes

--- snake/entities/snake.py
+++ snake/entities/snake.py
@@ -2,7 +2,6 @@
 """ Importing """
 
 from .  import Entity
-from .. import Tag
 
 """ Packaging """
 
@@ -10,20 +9,16 @@ __all__ = ['Snake', 'SnakeHead', 'SnakeBody']
 
 class Snake:
 
-    def __init__(self, messages, direction, cell):
+    def __init__(self, messages, cell):
         """ Create a new snake """
 
         self.messages = messages
 
-        self.head     = SnakeHead(direction=direction)
+        self.head     = SnakeHead(cell=cell)
         self.body     = []
-
-        self.head.move(cell=cell)
 
     def update(self, direction):
         """ Perform a snake update """
-
-        self.head.setDirection(direction=direction)
 
         present = self.head.get()
         future  = present.neighbour(direction=direction)
@@ -31,7 +26,7 @@ class Snake:
         if future is None:
             self.messages.put(lambda x: setattr(x, 'gameover', True), 'snake')
             return
-        elif future.has() and future.get().getTag() in [Tag.HEAD, Tag.BODY]:
+        elif future.hasEntity() and future.getEntity().doesBlock():
             self.messages.put(lambda x: setattr(x, 'gameover', True), 'snake')
             return
         else:
@@ -50,26 +45,8 @@ class Snake:
 
 class SnakeBody(Entity):
 
-    pass
+    blocks = True
 
 class SnakeHead(Entity):
 
-    def __init__(self, direction):
-        """ Create a new snake head entity 
-        
-        Parameters: TBC
-        """
-
-        super().__init__()
-        self.direction = direction
-
-    def setDirection(self, direction):
-        """ Set a snake head's direction 
-        
-        Parameters: TBC
-        """
-
-        self.direction = direction
-
-SnakeBody.setTag(Tag.BODY)
-SnakeHead.setTag(Tag.HEAD)
+    blocks = True
