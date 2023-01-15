bda3f4394bfa2747428acc92ea693c1f886e72b5
Sam Parker <s4m.prkr@gmail.com>
2023-01-15 20:51:50 +0000

    Output score after game over

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
 
 """ Importing """
 
-from game import *
+from game import Game, Controller
 from sys  import argv
 
 if __name__ == '__main__':
     if len(argv) != 3:
         raise RuntimeError(f'expected two command line arguments\n\tpython3 run.py [game-parameters] [display-parameters]')
+        
     game = Game(parameters=argv[1], controller=Controller, display=argv[2])
-    game.play()
+    score = game.play()
+    print(f'Score: {score}')
