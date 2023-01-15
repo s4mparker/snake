bd0a8b41129c588218a7a4b331f4f81819aced00
Sam Parker <s4m.prkr@gmail.com>
2023-01-15 19:53:03 +0000

    Tidied up Display class

--- game/in_out/display.py
+++ game/in_out/display.py
@@ -11,22 +11,31 @@ __all__ = ['Display']
 
 class Display:
 
-    def __init__(self, display_parameters):
+    def __init__(self, parameters):
         """ Create a new display 
         
-        Parameters: TBC
+        Parameters:
+            parameters (str) : display parameters
         """
 
-        params           = Parameters.import_file(filename=display_parameters)
+        # Display parameters
+        params           = Parameters.import_file(filename=parameters)
+        width, height    = params.get('WIDTH'), params.get('HEIGHT')
+        colours          = params.get('COLOURS')
         
-        if not get_init() : init()
-        self.screen      = set_mode((params.get('WIDTH'), params.get('HEIGHT')))
+        if not get_init(): 
+            init()
 
+        self.screen      = set_mode((width, height))
         self.components  = []
-        self.colours     = {name: Display.hexToRGB(colour=colour) for (name, colour) in params.get('COLOURS')}
+        self.colours     = {name: Display.hexToRGB(colour=colour) for (name, colour) in colours}
 
     def initialize(self, game):
-        """ Initialize a display """
+        """ Initialize a display 
+        
+        Parameters:
+            game (Game) : the game that will be rendered by the display
+        """
 
         w, h    = game.map.size()
         cw, ch  = self.screen.get_width() // w, self.screen.get_height() // h
@@ -42,19 +51,21 @@ class Display:
     def update(self):
         """ Update a display """
 
-        self.screen.fill(color=Display.hexToRGB('000000'))
+        white = Display.hexToRGB('000000')
+        black = Display.hexToRGB('FFFFFF')
+
+        self.screen.fill(color=white)
 
         for (cell, surface, rect) in self.components:
-            entity      = cell.getEntity()
-
-            if entity is None:
-                surface.fill(color=Display.hexToRGB('FFFFFF'))
+            if not cell:
+                surface.fill(color=black)
             else:
-                entity_type = type(entity).__name__
-                if entity_type in self.colours:
-                    surface.fill(color=self.colours.get(entity_type))
+                entity = type(cell.get()).__name__
+                colour = self.colours.get(entity, None)
+                if colour:
+                    surface.fill(color=colour)
                 else:
-                    raise KeyError(f'unrecognised colour provided ({entity_type})')
+                    raise KeyError(f'unrecognised colour ({entity})')
 
             self.screen.blit(surface, rect)
 
