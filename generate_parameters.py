16deae68a487a5b9b14f88d29102de1f211dc171
Sam Parker <s4m.prkr@gmail.com>
2023-01-15 20:50:58 +0000

    Updated parameters - will gitignore in future

--- generate_parameters.py
+++ generate_parameters.py
@@ -5,20 +5,20 @@ from game import Parameters
 
 if __name__ == '__main__':
     data = {
-        'WIDTH': 40,
-        'HEIGHT': 40,
-        'STARTX': 20,
-        'STARTY': 20,
-        'DIRECTION': 'left',
+        'WIDTH': 20,
+        'HEIGHT': 20,
+        'STARTX': 10,
+        'STARTY': 10,
+        'DIRECTION': 'right',
         'PERIOD': 0.1
     }
 
     Parameters.export_file(data, 'parameters/game.yaml')
 
     data = {
-        'WIDTH': 280,
-        'HEIGHT': 280,
-        'COLOURS' : [('SnakeHead', '0101FF'), ('SnakeEntity', '010188'), ('Apple', 'EE0101')]
+        'WIDTH': 600,
+        'HEIGHT': 600,
+        'COLOURS' : [('SnakeHead', '0101BF'), ('SnakeEntity', '89CFF0'), ('Apple', 'EE0101'), ('GoldenApple', 'FFD700')]
     }
 
     Parameters.export_file(data, 'parameters/display.yaml')
