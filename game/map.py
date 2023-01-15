6e332e5a2a99cd9e2c320be4cef01fa991d176e0
Sam Parker <s4m.prkr@gmail.com>
2023-01-15 19:02:15 +0000

    Split Map & Cell into two files - also renamed Cell to Tile

--- game/map.py
+++ game/map.py
@@ -1,113 +1,60 @@
 
 """ Importing """
 
-from .         import Direction, Sequencer
+from .         import Tile
 from itertools import product
 
 """ Packaging """
 
-__all__ = ['Map', 'Cell']
+__all__ = ['Map']
 
-class Map:
+class TileMap:
 
     def __init__(self, width, height):
-        """ Create a new map 
+        """ Create a new tile map
         
-        Parameters: TBC
+        Parameters:
+            width  (int) : map width (x)
+            height (int) : map height (y)
         """
 
         self.width  = width
         self.height = height
-        self.cells  = [[Cell() for y in range(height)] for x in range(width)]
 
+        self.tiles  = [[Tile(x=x, y=y) for y in range(height)] for x in range(width)]
         for (x, y) in product(range(width), range(height)):
-            left  = self.cells[x-1][y] if x > 0 else None
-            right = self.cells[x+1][y] if x < width - 1 else None
-            up    = self.cells[x][y-1] if y > 0 else None
-            down  = self.cells[x][y+1] if y < height - 1 else None
-            self.cells[x][y].localize(x, y, left, right, up, down)
+            self.tiles[x][y].localize(left=self.at(x-1, y), right=self.at(x+1, y), up=self.at(x, y-1), down=self.at(x, y+1))
 
     def __iter__(self):
-        """ Return an iterator over a map """
+        """ Return an iterator over a map's cells """
 
         return iter([cell for row in self.cells for cell in row])
 
-    def at(self, x, y):
+    def at(self, x, y, **kwargs):
         """ Return the cell at a given position within a map 
         
-        Parameters: TBC
+        Parameters:
+            x (int)     : pass
+            y (int)     : pass
+            kwargs      : keyword arguments
+                default : default value to return if no tile is found at the given position
         """
 
-        if x < 0 or x >= self.width:
-            raise IndexError(f'out of range')
-        if y < 0 or y >= self.width:
-            raise IndexError(f'out of range')
+        use_default = 'default' in kwargs
+        val_default = kwargs.pop('default', None)
+        
+        if len(kwargs) > 0:
+            raise KeyError(f'unrecognised keyword argument/s ({kwargs.keys()})')
 
-        return self.cells[x][y]
+        if x < 0 or x >= self.width or y < 0 or y >= self.width:
+            if use_default:
+                return val_default
+            else:
+                raise IndexError(f'out of range')
+        else:
+            return self.cells[x][y]
 
     def size(self):
         """ Return a map's size (W x H) """
 
         return self.width, self.height
-
-class Cell:
-
-    def __init__(self):
-        """ Create a new cell """
-
-        self.id         = Sequencer.generate()
-        self.entity     = None
-        self.x = self.y = None
-        self.neighbours = {
-            Direction.LEFT  : None,
-            Direction.RIGHT : None,
-            Direction.UP    : None,
-            Direction.DOWN  : None
-        }
-
-    def __str__(self):
-        """ Return a string representation of a cell """
-
-        return f'{self.id:^4}'
-
-    def __hash__(self):
-        """ Return a hash of a cell """
-
-        return hash(self.id)
-
-    def setEntity(self, entity):
-        """ Set a cell's entity """
-
-        self.entity = entity
-
-    def getEntity(self):
-        """ Get a cell's entity """
-
-        return self.entity
-
-    def hasEntity(self):
-        """ Return True / False based on whether a cell contains an entity """
-
-        return self.entity is not None
-
-    def clearEntity(self):
-        """ Clear a cell's entity """
-
-        self.entity = None
-
-    def localize(self, x, y, left, right, up, down):
-        """ Localize a cell in order to connect it to its neighbours """
-
-        self.x, self.y                   = x, y
-        self.neighbours[Direction.LEFT]  = left
-        self.neighbours[Direction.RIGHT] = right
-        self.neighbours[Direction.UP]    = up
-        self.neighbours[Direction.DOWN]  = down
-
-    def neighbour(self, direction):
-        """ Return a cell's neighbour in a particular direction """
-
-        return self.neighbours[direction]
-
-
-
---
+++ game/tile.py
@@ -0,0 +1,78 @@
+
+""" Importing """
+
+from . import Direction, Sequencer
+
+""" Packaging """
+
+__all__ = ['Tile']
+
+class Tile:
+
+    def __init__(self, x, y):
+        """ Create a new tile 
+        
+        Parameters:
+            x (int) : tile's x co-ordinate
+            y (int) : tile's y co-ordinate
+        """
+
+        self.id         = Sequencer.generate()
+        self.entity     = None
+        self.x = self.y = x, y
+        self.neighbours = {k: None for k in Direction}
+
+    def __str__(self):
+        """ Return a string representation of a tile """
+
+        return f'{self.id:^4}'
+
+    def __hash__(self):
+        """ Return a hash of a tile """
+
+        return hash(self.id)
+
+    def __bool__(self):
+        """ Determine whether a tile has been assigned an entity """
+
+        return self.entity is not None
+
+    def set(self, entity):
+        """ Assign a tile an entity
+        
+        Parameters:
+            entity (Entity) : entity to be associated with the tile
+        """
+
+        self.entity = entity
+
+    def get(self):
+        """ Get a tile's assigned entity """
+
+        return self.entity
+
+    def clear(self):
+        """ Clear a tile's assigned entity """
+
+        self.entity = None
+
+    def localize(self, left, right, up, down):
+        """ Localize a tile in order to connect it to its neighbouring tiles
+        
+        Parameters:
+            left  (Tile) : tile's 'left' neighbour
+            right (Tile) : tile's 'right' neighbour
+            up    (Tile) : tile's 'up' neighbour
+            down  (Tile) : tile's 'down' neighbour
+        """
+
+        self.neighbours.update({Direction.LEFT: left, Direction.RIGHT: right, Direction.UP: up, Direction.DOWN: down})
+
+    def neighbour(self, direction):
+        """ Return a tile's neighbour in a particular direction 
+        
+        Parameters:
+            direction (Direction) : the direction in which to get the neighbour
+        """
+
+        return self.neighbours[direction]
