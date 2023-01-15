c702b46ed3b867bf2674088762733be8f23503f7
Sam Parker <s4m.prkr@gmail.com>
2023-01-15 20:00:44 +0000

    Removing final traces of Messages class

--- game/utilities/__init__.py
+++ game/utilities/__init__.py
@@ -6,6 +6,5 @@ pass
 """ Packaging """
 
 from .enums      import *
-from .messages   import *
 from .parameters import *
 from .sequencer  import *
--- game/utilities/enums.py
+++ game/utilities/enums.py
@@ -5,7 +5,7 @@ from enum import Enum
 
 """ Packaging """
 
-__all__ = ['Direction', 'Message']
+__all__ = ['Direction']
 
 class Direction(Enum):
     LEFT  = 'left'
