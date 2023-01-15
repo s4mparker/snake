450ac21a52c236b8380708f902774cb12e4d9adf
Sam Parker <s4m.prkr@gmail.com>
2023-01-15 19:52:01 +0000

    Removed messaging functionality

--- game/utilities/enums.py
+++ game/utilities/enums.py
@@ -12,20 +12,3 @@ class Direction(Enum):
     RIGHT = 'right'
     UP    = 'up'
     DOWN  = 'down'
-
-class Message(Enum):
-    GAMEOVER = lambda o: setattr(o, 'game_active', False)
-    LENGTHEN = 'lengthen'
-    NEWAPPLE = 'new_apple'
-    ADD10    = lambda o: setattr(o, 'score', o.score + 10)
-
-    def __call__(self, *args):
-        """ Execute a message 
-        
-        Parameters: TBC
-        """
-
-        if callable(self.value):
-            self.value(*args)
-        
-    
